5df88d374651e1e37b757e98de4736417e28f5ff
kakulya14 <nbartoshuk@mail.ru>
2023-04-10 01:51:37 +0300

    added controllers logic

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,13 +3,14 @@ const ApiError = require('../error/ApiError')
 
 class BlogController {
     async create(req, res) {
-        const {name} = req.body
-        const blog = await Blog.create({name})
+        const {title, description, } = req.body
+        const blog = await Blog.create({title, description})
         return res.json(blog)
     }
 
     async getAll(req, res) {
-
+        const blogs = await Blog.findAndCountAll()
+        return res.json(blogs)
     }
 
 }
--- models/models.js
+++ models/models.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize')
 
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    phone: {type: DataTypes.INTEGER, unique: true},
+    email: {type: DataTypes.INTEGER, unique: true},
     password: {type: DataTypes.STRING},
     role: {type: DataTypes.STRING, defaultValue: "USER"}
 })
@@ -17,8 +17,16 @@ const Shop = sequelize.define('shop', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING},
     description: {type: DataTypes.STRING},
-    // image: {type: DataTypes.BLOB}
+    img: {type: DataTypes.STRING}
 })
+
+Shop.sync()
+    .then(() => {
+        console.log('Модель синхронизирована с базой данных');
+    })
+    .catch((error) => {
+        console.error('Ошибка синхронизации модели: ', error);
+    });
 
 const Vacancies = sequelize.define('vacancies', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
